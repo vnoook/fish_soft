bc65600914eb1cbce73be17d704b1c6fb00d046d
vnoook <noook@yandex.ru>
2021-10-01 00:12:11 +0700

    #4

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 # вспомогательные данные для расчётов консольной версии
 # таблица рыбаков с id и fio
-table_anglers = {1:'Иванов', 2:'Петров', 3:'Сидоров', 4:'Фролов', 5:'Попов', 6:'Котов', 7:'Рыбов', 8:'Лотов'}
+table_anglers = {1: 'Иванов', 2: 'Петров', 3: 'Ветров', 4: 'Фролов',
+                 5: 'Попков', 6: 'Котова', 7: 'Рыбова', 8: 'Локтев'}
 # таблица рыбаков с id и разрядами
-table_anglers_rank = {1:'1 разряд', 2:'2 разряд', 3:'3 разряд', 4:'1 разряд', 5:'3 разряд', 6:'МС', 7:'КМС', 8:'МС'}
+table_anglers_rank = {1: '1 разряд', 2: '2 разряд', 3: '3 разряд', 4: '1 разряд',
+                      5: '3 разряд', 6: 'МС', 7: 'КМС', 8: 'МС'}
 # таблица поимок в четырёх периодах
-table_catches = {1:[1,4,7,1], 2:[2,52,2,2], 3:[43,4,5,6], 4:[1,0,4,22], 5:[8,0,12,71], 6:[15,18,1,0], 7:[0,16,13,3], 8:[5,17,4,4]}
+table_catches = {1: [1, 4, 7, 1], 2: [2, 52, 2, 2], 3: [43, 4, 5, 6], 4: [1, 0, 4, 22],
+                 5: [8, 0, 12, 71], 6: [15, 18, 1, 0], 7: [0, 16, 13, 3], 8: [5, 17, 4, 4]}
 table_tournament = ''
+
 
 # класс Рыболов
 class Angler:
-    def __init__(self, id, fio, rank, team=None):
-        self.angler_id = id
-        self.angler_fio = fio
-        self.angler_rank = rank
-        self.angler_team = team
-    def angler_info(self):
-        print(f'{self.angler_id} ... {self.angler_fio}')
+    def __init__(self, a_id, a_fio, a_rank, a_team=None):
+        self.angler_id = a_id
+        self.angler_fio = a_fio
+        self.angler_rank = a_rank
+        self.angler_team = a_team
+
+    def angler_info_all(self):
+        a_team = 'Личник' if self.angler_team is None else self.angler_team
+        print(f'{self.angler_id} . {self.angler_fio} . {self.angler_rank} . {a_team}')
+
+    def angler_info_id(self):
+        print(f'{self.angler_fio} = {self.angler_id}')
+
+    def angler_info_fio(self):
+        print(f'{self.angler_id} = {self.angler_fio}')
+
+    def angler_info_rank(self):
+        print(f'{self.angler_fio} = {self.angler_rank}')
+
+    def angler_info_team(self):
+        if self.angler_team is None:
+            print(f'{self.angler_fio} в команде, он Личник')
+        else:
+            print(f'{self.angler_fio} в команде {self.angler_team}')
+
 
 if __name__ == '__main__':
     print()
 
     # создание экземпляров рыболовов по количеству в table_anglers
-    for angler in table_anglers:
-        pass
+    for angler_id, angler_fio in table_anglers.items():
+        print(f'Angler{angler_id} ... {angler_fio} ... {table_anglers_rank[angler_id]}')
+        # string_angler_class = 'Angler'+angler_id
+        # globals()[string_angler_class] = Angler(angler_id, angler_fio)
 
     print()
