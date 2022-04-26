24dc1c49693a49b0734d8082352732f3fd1b8414
vnoook <noook@yandex.ru>
2022-04-26 17:01:01 +0700

    #36 add calculation coordinates of fields

--- calc_coods.py
+++ calc_coods.py
@@ -1,30 +1,33 @@
 #точка начала отчёта
 start_dot_x = 5
 start_dot_y = 100
+
 # растояние между объектами на форме
 gap_x = 20
 gap_y = 10
+
 # стандартная высота для всех полей
 field_height = 20
+
 # кортеж из полей на форме "Название поля", длина
 fields = (('Sportik01_number_', 40),
           ('Sportik02_lottery_', 40),
-          ('Sportik03_fio_', 180)
-          # ,
-          # ('Sportik04_team_', 180),
-          # ('Sportik05_rank_', 40),
-          # ('Sportik06_zona1_', 70),
-          # ('Sportik07_zona2_', 70),
-          # ('Sportik08_period1_', 40),
-          # ('Sportik09_period2_', 40),
-          # ('Sportik10_period3_', 40),
-          # ('Sportik11_period4_', 40),
-          # ('Sportik12_points_', 40),
-          # ('Sportik13_team_place_', 40),
-          # ('Sportik14_self_place_', 40)
+          ('Sportik03_fio_', 180),
+          ('Sportik04_team_', 180),
+          ('Sportik05_rank_', 40),
+          ('Sportik06_zona1_', 70),
+          ('Sportik07_zona2_', 70),
+          ('Sportik08_period1_', 40),
+          ('Sportik09_period2_', 40),
+          ('Sportik10_period3_', 40),
+          ('Sportik11_period4_', 40),
+          ('Sportik12_points_', 40),
+          ('Sportik13_team_place_', 40),
+          ('Sportik14_self_place_', 40)
          )
+
 # количество спортиков
-q_sportiks = 13
+q_sportiks = 3
 
 field_step_y = start_dot_y
 for sportik in range(1, q_sportiks+1):
@@ -36,10 +39,12 @@ for sportik in range(1, q_sportiks+1):
         print(f'[{field_step_x},{field_step_y}] {field_name} ({field_width} {field_height})', end=' ')
 
         field_step_x = field_step_x + field_width
-        print(f'[{field_step_x},{field_step_y}]', end=' ... ')
+        print(f'<{field_step_x},{field_step_y}>', end=' ... ')
 
         field_step_x = field_step_x + gap_x
         print(f'{gap_x}', end=' ... ')
+
+    print(f'{field_step_x}', end='')
 
     field_step_y = field_step_y + field_height + gap_y
 
