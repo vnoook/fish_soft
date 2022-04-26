3d0ab82b24339a8c027b7e9f4de67814ee4d59c9
vnoook <noook@yandex.ru>
2022-04-26 16:19:44 +0700

    #35 add calculation coordinates of fields

--- calc_coods.py
+++ calc_coods.py
@@ -1,17 +1,15 @@
 #точка начала отчёта
 start_dot_x = 5
 start_dot_y = 100
-
 # растояние между объектами на форме
 gap_x = 20
 gap_y = 10
-
 # стандартная высота для всех полей
 field_height = 20
 # кортеж из полей на форме "Название поля", длина
 fields = (('Sportik01_number_', 40),
           ('Sportik02_lottery_', 40),
-          ('Sportik03_fio_', 180))
+          ('Sportik03_fio_', 180)
           # ,
           # ('Sportik04_team_', 180),
           # ('Sportik05_rank_', 40),
@@ -23,27 +21,27 @@ fields = (('Sportik01_number_', 40),
           # ('Sportik11_period4_', 40),
           # ('Sportik12_points_', 40),
           # ('Sportik13_team_place_', 40),
-          # ('Sportik14_self_place_', 40))
-
+          # ('Sportik14_self_place_', 40)
+         )
 # количество спортиков
-q_sportiks = 3
+q_sportiks = 13
 
+field_step_y = start_dot_y
 for sportik in range(1, q_sportiks+1):
-    out_str = ''
-    field_step_x = 0
+    field_step_x = start_dot_x
     for field in fields:
         field_name = field[0] + str(sportik)
         field_width = field[1]
 
-        print(fields, field, field_name, field_width, field_height)
+        print(f'[{field_step_x},{field_step_y}] {field_name} ({field_width} {field_height})', end=' ')
 
-        out_str = out_str + field_name + ' ... '
+        field_step_x = field_step_x + field_width
+        print(f'[{field_step_x},{field_step_y}]', end=' ... ')
 
-    print(out_str)
+        field_step_x = field_step_x + gap_x
+        print(f'{gap_x}', end=' ... ')
 
+    field_step_y = field_step_y + field_height + gap_y
 
-
-
-
-
+    print()
 
