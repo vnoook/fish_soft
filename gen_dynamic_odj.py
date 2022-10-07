a84fb0c88c1b3d8fa7377add2ac9cd6897037ea5
vnoook <noook@yandex.ru>
2022-10-07 19:47:54 +0700

    #48 add experiment file for dynamic adder

--- gen_dynamic_odj.py
+++ gen_dynamic_odj.py
@@ -1,27 +1,73 @@
+import pprint
 import sys
-from PyQt5 import QtWidgets, QtGui, QtCore
+import PyQt5
+import PyQt5.QtWidgets
+import PyQt5.QtCore
+import PyQt5.QtGui
+import pprint
 
-class Window(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
 
-        self.vlayout = QtWidgets.QVBoxLayout()
-        self.pushButton_ok = QtWidgets.QPushButton("Press me", self)
-        self.pushButton_ok.clicked.connect(self.addCheckbox)
-        self.vlayout.addWidget(self.pushButton_ok)
+class Window(PyQt5.QtWidgets.QWidget):
 
-        self.checkBox = QtWidgets.QCheckBox(self)
-        self.vlayout.addWidget(self.checkBox)
+    checkbox_number = 0
+
+    def __init__(self):
+        super().__init__()
+
+        self.vlayout = PyQt5.QtWidgets.QVBoxLayout()
+
+        self.pushButton_add = PyQt5.QtWidgets.QPushButton('+', self)
+        self.pushButton_add.clicked.connect(self.add_checkbox)
+
+        self.pushButton_del = PyQt5.QtWidgets.QPushButton('-', self)
+        self.pushButton_del.clicked.connect(self.del_checkbox)
+
+        self.vlayout.addWidget(self.pushButton_add)
+        self.vlayout.addWidget(self.pushButton_del)
+
+        # self.checkBox = PyQt5.QtWidgets.QCheckBox(self)
+        # self.vlayout.addWidget(self.checkBox)
         self.setLayout(self.vlayout)
 
-    def addCheckbox(self):
-        #checkBox = QtWidgets.QCheckBox()
-        self.vlayout.addWidget(QtWidgets.QCheckBox()) 
+    def add_checkbox(self):
+        Window.checkbox_number = Window.checkbox_number + 1
+        print(Window.checkbox_number)
+        checkbox_name = 'checkbox_' + str(Window.checkbox_number)
 
-application = QtWidgets.QApplication(sys.argv)
-window = Window()
-window.setWindowTitle('Dynamically adding checkboxes using a push button')
-window.resize(250, 180)
-window.show()
-sys.exit(application.exec_())
+        self.checkBox = PyQt5.QtWidgets.QCheckBox()
+        self.checkBox.setObjectName(checkbox_name)
+        self.checkBox.setText(checkbox_name)
+        self.checkBox.clicked.connect(self.click_checkbox)
 
+        pprint.pprint(self.__dict__)
+        # self.dict_obj = []
+        # self.dict_obj[checkbox_name] = self.checkBox
+        # print()
+
+        self.vlayout.addWidget(self.checkBox)
+
+    def del_checkbox(self):
+        if Window.checkbox_number > 0:
+            Window.checkbox_number = Window.checkbox_number - 1
+            print(Window.checkbox_number)
+        pass
+
+    def click_checkbox(self):
+        print(self.objectName())
+        pass
+
+
+# создание основного окна
+def main_app():
+    application = PyQt5.QtWidgets.QApplication(sys.argv)
+    application.setStyle('Fusion')
+    window = Window()
+    window.setWindowTitle('Динамическое добавление объектов')
+    window.resize(400, 300)
+    window.show()
+    sys.exit(application.exec_())
+
+
+# запуск основного окна
+if __name__ == '__main__':
+    main_app()
