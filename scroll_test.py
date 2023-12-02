a21512cda74981728ca76d0e7515efbf2b869414
vnoook <noook@yandex.ru>
2023-12-02 09:31:04 +0700

    #353 experiments with form`s objects

--- scroll_test.py
+++ scroll_test.py
@@ -1,22 +1,27 @@
 # 385 in file main.py
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QScrollArea, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QScrollArea, QMainWindow, QLineEdit
 
 
 class ScrollableButtonWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.init_ui()
-
-    def init_ui(self):
+    #     self.init_ui()
+    #
+    # def init_ui(self):
         scroll = QScrollArea()
         widget = QWidget()
         layout = QVBoxLayout()
 
-        for i in range(20):
+        for i in range(9):
+            # QPushButton
             button = QPushButton(f"Button {i+1}")
             layout.addWidget(button)
+            # QLineEdit
+            line_edit = QLineEdit(self)
+            line_edit.setGeometry(10, 10, 20, 20)
+            layout.addWidget(line_edit)
 
         widget.setLayout(layout)
         scroll.setWidgetResizable(True)
@@ -24,7 +29,7 @@ class ScrollableButtonWindow(QMainWindow):
 
         self.setCentralWidget(scroll)
         self.setWindowTitle('Scrollable Button Window')
-        self.setGeometry(100, 100, 300, 400)
+        self.setGeometry(100, 100, 500, 500)
 
 
 if __name__ == '__main__':
