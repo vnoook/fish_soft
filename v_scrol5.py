d4d0b60c3f34de50c5a7ecbd713aaf2a6c3d175f
vnoook <noook@yandex.ru>
2023-12-10 17:05:05 +0700

    #356 experiments with form`s objects

--- v_scrol5.py
+++ v_scrol5.py
@@ -1,20 +1,54 @@
 import sys
 from PyQt5.Qt import *
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle('Title MainWindow')
+        self.resize(500, 300)
+
+        # self.centralwidget = QWidget(MainWindow)
+        # self.centralwidget.setObjectName(u"centralwidget")
+        # self.widget = QWidget(self.centralwidget)
+        # self.widget.setObjectName(u"widget")
+        # self.widget.setGeometry(QRect(20, 20, 381, 301))
+        # MainWindow.setCentralWidget(self.centralwidget)
+        # MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        # QMetaObject.connectSlotsByName(MainWindow)
+
         self.widget = QWidget()
+        self.widget.resize(100, 100)
         self.vbox = QVBoxLayout(self.widget)
 
-        for i in range(20):
+        w = QWidget()
+        hbox = QHBoxLayout(w)
+        label = QLabel(f'Label---')
+        lineEdit = QLineEdit(f'LineEdit---')
+        button = QPushButton(f'Button---')
+        hbox.addWidget(label)
+        hbox.addWidget(lineEdit)
+        hbox.addWidget(button)
+        self.vbox.addWidget(w)
+
+        w = QWidget()
+        hbox = QHBoxLayout(w)
+        label1 = QLabel(f'Label---1')
+        label2 = QLabel(f'Label---2')
+        label3 = QLabel(f'Label---3')
+        hbox.addWidget(label1)
+        hbox.addWidget(label2)
+        hbox.addWidget(label3)
+        self.vbox.addWidget(w)
+
+        for i in range(1, 20+1):
             w = QWidget()
             hbox = QHBoxLayout(w)
+
             label = QLabel(f'Label {i}')
             lineEdit = QLineEdit(f'LineEdit {i}')
             button = QPushButton(f'Button {i}')
+
             hbox.addWidget(label)
             hbox.addWidget(lineEdit)
             hbox.addWidget(button)
@@ -36,7 +70,7 @@ class MainWindow(QMainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = MainWindow()
-    main.setWindowTitle('Title MainWindow')
-    main.resize(500, 300)
+    # main.setWindowTitle('Title MainWindow')
+    # main.resize(500, 300)
     main.show()                    
     sys.exit(app.exec_())
