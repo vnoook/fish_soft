794912d318c873e91ba671c244b433ea687c3641
noook <noook@yandex.ru>
2024-10-01 15:49:47 +0700

    #365 rewrite v_scrol2.py

--- v_scrol2.py
+++ v_scrol2.py
@@ -1,55 +1,48 @@
 import sys
 import PyQt5
-from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QScrollBar, QVBoxLayout, QCheckBox, QHBoxLayout, QGridLayout, QScrollArea)
-from PyQt5.QtCore import Qt, QSize
+import PyQt5.QtWidgets
+import PyQt5.QtCore
+import PyQt5.QtGui
 
-class Example(QWidget):
+
+class Example(PyQt5.QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
         # Create main layout
-        main_layout = QHBoxLayout(self)
+        main_layout = PyQt5.QtWidgets.QHBoxLayout(self)
+
         # Create widget to hold checkboxes
-        self.checkbox_widget = QWidget()
-        checkbox_layout = QGridLayout(self.checkbox_widget)
+        self.checkbox_widget = PyQt5.QtWidgets.QWidget()
+        checkbox_layout = PyQt5.QtWidgets.QGridLayout(self.checkbox_widget)
+
         # Create checkboxes (5 columns x 100 rows)
         self.checkboxes = []
-        for row in range(50):
-            for col in range(50):
-                checkbox = QCheckBox(f"{row * 5 + col + 1}", self)
+        for row in range(20):
+            for col in range(20):
+                checkbox = PyQt5.QtWidgets.QCheckBox(f"{row * 5 + col + 1}", self)
                 self.checkboxes.append(checkbox)
-                checkbox.setFixedSize(QSize(15, 15))  # Set checkbox size
+                checkbox.setFixedSize(PyQt5.QtCore.QSize(15, 15))  # Set checkbox size
                 checkbox_layout.addWidget(checkbox, row, col)
-        # Create vertical scroll bar
-        # self.scroll = QScrollBar(Qt.Vertical, self) !!!!!
-        # self.scroll.setMaximum(100)  # Adjust based on your needs !!!!
-        # self.scroll.valueChanged.connect(self.scrollValueChanged) !!!!!
-        # Create layout for scroll bar
-        # scroll_layout = QVBoxLayout() !!!!!
-        # scroll_layout.addWidget(self.scroll)  !!!!
+
         # Create scroll area to hold checkboxes widget
         self.scroll_area = PyQt5.QtWidgets.QScrollArea(self)
         self.scroll_area.setWidget(self.checkbox_widget)
         self.scroll_area.setWidgetResizable(False)  # Prevent resizing of checkbox widget
+
         # Add scroll area and scroll bar to main layout
         main_layout.addWidget(self.scroll_area)
-        # main_layout.addLayout(scroll_layout) !!!!!
+
         # Set layout and window properties
         self.setLayout(main_layout)
         self.setGeometry(300, 300, 700, 600)  # Set form size
         self.setWindowTitle('QScrollBar')
         self.show()
 
-    def scrollValueChanged(self, value):
-        # Adjust the position of checkboxes based on scroll value
-        # (No need to modify checkboxes' visibility in this case,
-        # as the scroll area will handle it automatically)
-        pass
-
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = PyQt5.QtWidgets.QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
